00c39ff4fa7a89da115d8045b9ed33ad57778965
Peter Nowee <peter@peternowee.com>
2023-06-30 16:12:36 +0200

    Minor: Reorder/formatting resources

--- config/resources/domain.lisp
+++ config/resources/domain.lisp
@@ -1,5 +1,13 @@
 (in-package :mu-cl-resources)
 
+(define-resource person ()
+  :class (s-prefix "schema:Person")
+  :properties `((:name :string ,(s-prefix "schema:name")))
+  :has-many `((individual-product :via ,(s-prefix "schema:owns")
+                                  :as "individual-products"))
+  :resource-base (s-url "http://mu.semte.ch/examples/hardware/persons/")
+  :on-path "persons")
+
 (define-resource individual-product ()
   :class (s-prefix "schema:IndividualProduct")
   :properties `((:serial-number :string ,(s-prefix "schema:serialNumber"))
@@ -9,14 +17,6 @@
                      :as "person"))
   :resource-base (s-url "http://mu.semte.ch/examples/hardware/individual-products/")
   :on-path "individual-products")
-
-(define-resource person ()
-  :class (s-prefix "schema:Person")
-  :properties `((:name :string ,(s-prefix "schema:name")))
-  :has-many `((individual-product :via ,(s-prefix "schema:owns")
-                    :as "individual-products"))
-  :resource-base (s-url "http://mu.semte.ch/examples/hardware/persons/")
-  :on-path "persons")
 
 ;; reading in the domain.json
 (read-domain-file "domain.json")
