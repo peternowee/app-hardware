35c049845ae29241f5075020d26153e0564e177b
Peter Nowee <peter@peternowee.com>
2023-10-03 18:21:02 +0200

    delta-notifier from file-service to virus-scanner

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -12,5 +12,22 @@ export default [
       gracePeriod: 250,
       ignoreFromSelf: true
     }
+  },
+  {
+    match: {
+      predicate: {
+        type: 'uri',
+        value: 'http://www.semanticdesktop.org/ontologies/2007/01/19/nie#dataSource'
+      },
+    },
+    callback: {
+      url: 'http://virus-scanner/delta',
+      method: 'POST',
+    },
+    options: {
+      resourceFormat: 'v0.0.1',
+      gracePeriod: 1000,
+      ignoreFromSelf: true,
+    }
   }
 ];
